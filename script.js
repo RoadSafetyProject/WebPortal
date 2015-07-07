21622a5dd2a2ff66e1c97086f92bf3c001a862c7
John Francis Mukulu <john.f.mukulu@gmail.com>
2015-07-07 13:50:39 +0300

    Fixed tables display and reduced server address hard-coding

--- script.js
+++ script.js
@@ -5,10 +5,10 @@
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
 // Define the string
-var string = 'Hello World!';
+var serverUrl="http://roadsafety.go.tz/demo";
 
 // Encode the String
-var encodedString = Base64.encode("admin:IROAD2015");
+var encodedString = Base64.encode("portal:PORTAL2015");
 console.log(encodedString); // Outputs: "SGVsbG8gV29ybGQh"
 
 // Decode the String
@@ -19,7 +19,7 @@ $(document).ready(function(){
 
     $.ajax({
                     type: 'GET',
-                    url: 'http://roadsafety.go.tz/demo/api/dashboards.json?fields=id,name,href,dashboardItems[id,type,shape]&paging=false',
+                    url: serverUrl+'/api/dashboards.json?fields=id,name,href,dashboardItems[id,type,shape]&paging=false',
                     headers: {
                         "Authorization": "Basic " + encodedString,
                         "Content-Type": "application/json"
@@ -41,28 +41,40 @@ $(document).ready(function(){
                                         if(ds.shape == "normal")
                                             $.ajax({
                                                 type: 'GET',
-                                                url: 'http://roadsafety.go.tz/demo/api/dashboardItems/'+ds.id,
+                                                url: serverUrl+'/api/dashboardItems/'+ds.id,
                                                 headers: {
                                                     "Authorization": "Basic " + encodedString,
                                                     "Content-Type": "application/json"
                                                 },
                                                 success : function(data) {
                                                     var imgurl = data[data.type].href+'/data'
-                                                    $(".contents").append("<div class='col-sm-6' style=''><img src='"+imgurl+"' style='width: 100%' class='thumbnail responsive'></div>" );
+                                                    if(data.type=='reportTable') {
+														$.get(imgurl+'.html+css',function(htmldata){
+															$(".contents").append("<div class='col-sm-6'><div style='width: 100%' class='thumbnail responsive'>"+htmldata+"</div></div>" );
+														});
+													}else {
+														$(".contents").append("<div class='col-sm-6' style=''><img src='"+imgurl+"' style='width: 100%' class='thumbnail responsive'></div>" );
+													}
                                                 }
 
                                             });
                                         if(ds.shape == "double_width" || ds.shape == "full_width" )
                                             $.ajax({
                                                 type: 'GET',
-                                                url: 'http://roadsafety.go.tz/demo/api/dashboardItems/'+ds.id,
+                                                url: serverUrl+'/api/dashboardItems/'+ds.id,
                                                 headers: {
                                                     "Authorization": "Basic " + encodedString,
                                                     "Content-Type": "application/json"
                                                 },
                                                 success : function(data) {
                                                     var imgurl = data[data.type].href+'/data'
-                                                    $(".contents").append("<div class='col-sm-12' style=''><img src='"+imgurl+"' style='width: 100%' class='thumbnail responsive'></div>" );
+                                                     if(data.type=='reportTable') {
+														$.get(imgurl+'.html+css',function(htmldata){
+															$(".contents").append("<div class='col-sm-12'><div style='width: 100%' class='thumbnail responsive'>"+htmldata+"</div></div>" );
+														});
+													}else {
+														$(".contents").append("<div class='col-sm-12' style=''><img src='"+imgurl+"' style='width: 100%' class='thumbnail responsive'></div>" );
+													}
                                                 }
 
                                             });
